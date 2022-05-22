1f7fe9dc854d76c91a27227481389ce60922acbf
Bogdan Miron <bogdanmiron14@gmail.com>
2022-05-22 10:19:33 +0200

    Detected Colors + Tests

--- project.py
+++ project.py
@@ -1,3 +1,4 @@
+from asyncio.windows_events import NULL
 import pyrealsense2 as rs
 import numpy as np
 import cv2
@@ -165,11 +166,23 @@ try:
         frame_hsv = cv2.cvtColor(color_image, cv2.COLOR_BGR2HSV)
 
         x_blue, y_blue, z_blue = get_color(frame_hsv, blue_lower_range, blue_upper_range)
+        if x_blue != NULL and y_blue != NULL and z_blue != NULL:
+            print(f"Object blue detected and has the coordinates x:{x_blue}, y:{y_blue}, z:{z_blue}")
         x_red, y_red, z_red = get_color(frame_hsv, red_lower_range, red_upper_range)
+        if x_red != NULL and y_red != NULL and z_red != NULL:
+            print(f"Object red detected and has the coordinates x:{x_red}, y:{y_red}, z:{z_red}")
         x_green, y_green, z_green = get_color(frame_hsv, green_lower_range, green_upper_range)
+        if x_green != NULL and y_green != NULL and z_green != NULL:
+            print(f"Object green detected and has the coordinates x:{x_green}, y:{y_green}, z:{z_green}")
         x_orange, y_orange, z_orange = get_color(frame_hsv, orange_lower_range, orange_upper_range)
+        if x_orange != NULL and y_orange != NULL and z_orange != NULL:
+            print(f"Object orange detected and has the coordinates x:{x_orange}, y:{y_orange}, z:{z_orange}")
         x_yellow, y_yellow, z_yellow = get_color(frame_hsv, yellow_lower_range, yellow_upper_range)
+        if x_yellow != NULL and y_yellow != NULL and z_blue != NULL:
+            print(f"Object yellow detected and has the coordinates x:{x_yellow}, y:{y_yellow}, z:{y_yellow}")
         x_purple , y_purple, z_purple = get_color(frame_hsv, purple_lower_range, purple_upper_range)
+        if x_purple != NULL and y_purple != NULL and z_purple != NULL:
+            print(f"Object purple detected and has the coordinates x:{x_purple}, y:{y_purple}, z:{z_purple}")
       
         
 ############################################### Get 3D coordinates #####################################################################
--- webcamtest.py
+++ webcamtest.py
@@ -7,13 +7,14 @@ import cv2
 red_lower_range = np.array([0, 50, 50])
 red_upper_range = np.array([10, 255, 255])
 
-# orange - problems
+# orange
 orange_lower_range = np.array([11, 50, 50])
-orange_upper_range = np.array([29, 255, 255])
+orange_upper_range = np.array([20, 255, 255])
 
 # yellow
-yellow_lower_range = np.array([30, 50, 50])
-yellow_upper_range = np.array([40, 255, 255])
+yellow_lower_range = np.array([21, 50, 50])
+yellow_upper_range = np.array([49, 255, 255])
+
 
 # green
 green_lower_range = np.array([50, 50, 50])
@@ -24,7 +25,7 @@ blue_lower_range = np.array([140, 75, 50])
 blue_upper_range = np.array([180, 255, 255])  
 
 # purple
-purple_lower_range = np.array([200, 50, 50])
+purple_lower_range = np.array([190, 50, 50])
 purple_upper_range = np.array([240, 255, 255])  
 
 
@@ -35,10 +36,21 @@ def get_color(img_hsv, lower, upper):
     center_points = list()
     cv2.drawContours(img, cn, -1, (0,255,0), 3)
     i = 0
-     # print(len(cn))
+    # if lower == red_lower_range and upper == red_upper_range:
+    #     print("detected red ")
+    # elif lower == blue_lower_range and upper == blue_lower_range:
+    #     print("detect blue ")
+    # elif lower == green_lower_range and upper == green_upper_range:
+    #     print("detect green ")
+    # elif lower == purple_lower_range and upper == purple_upper_range:
+    #     print("detect purple ")
+    # elif lower == orange_lower_range and upper == orange_upper_range:
+    #     print("detected orange ")
+    # elif lower == yellow_lower_range and upper ==yellow_upper_range:
+    #     print("detected yellow ")
     while i < len(cn):
         M = cv2.moments(cn[i])
-        if (M['m00']!=0):
+        if (M['m00']!=0 and M['m00']>2000 and M['m00']<7000):
             cx = int(M['m10']/M['m00'])
             cy = int(M['m01']/M['m00'])
             # center_points += [int(xg+np.floor(wg/2)), int(yg+np.floor(hg/2))]
@@ -51,26 +63,28 @@ def get_color(img_hsv, lower, upper):
 
 # TESTING PHASE
 # Don't forget to set the right path 
-img = cv2.imread("image_colors/test_robot_color5.jpeg")   
-img = cv2.copyMakeBorder(img, 40, 40, 40, 40, cv2.BORDER_REPLICATE)
+img = cv2.imread("image_colors/test_pictures_2022_05_21/data/data6.jpeg")   
+# img = cv2.copyMakeBorder(img, 40, 40, 40, 40, cv2.BORDER_REPLICATE)
 
-scale_percent = 50 # percent of original size
-width = int(img.shape[1] * scale_percent / 100)
-height = int(img.shape[0] * scale_percent / 100)
-dim = (width, height)
+# scale_percent = 50 # percent of original size
+# width = int(img.shape[1] * scale_percent / 100)
+# height = int(img.shape[0] * scale_percent / 100)
+# dim = (width, height)
 
-img = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+# img = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
 img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV_FULL)
 
 
-get_color( img_hsv, red_lower_range, red_upper_range)
-get_color( img_hsv, purple_lower_range, purple_upper_range)
-get_color( img_hsv, green_lower_range, green_upper_range)
-get_color(img_hsv, yellow_lower_range, yellow_upper_range)
-get_color(img_hsv, blue_lower_range,blue_upper_range)
+# get_color(img_hsv, red_lower_range, red_upper_range)
+# get_color(img_hsv, purple_lower_range, purple_upper_range)
+# get_color(img_hsv, green_lower_range, green_upper_range)
+# get_color(img_hsv, yellow_lower_range, yellow_upper_range)
+# get_color(img_hsv, blue_lower_range,blue_upper_range)
 get_color(img_hsv, orange_lower_range, orange_upper_range)
 cv2.imshow("imga", img)
-
+# print("detected red detected purple detected green detected yellow detected blue detected orange ")
+print("detected orange ")
+cv2.imwrite("image_colors/test_pictures_2022_05_21/results/data_color6.jpeg", img)
 cv2.waitKey(0)
 
 '''
